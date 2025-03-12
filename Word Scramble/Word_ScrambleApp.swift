434be89db2bb91b2ec6596c8bbac75658b57fbeb
MoniqueFer <138914572+MoniqueFer@users.noreply.github.com>
2025-03-12 11:30:15 -0300

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Monique Ferrarini on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
